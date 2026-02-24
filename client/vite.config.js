c896dcfd693605ddd821b2c57bfd9e8bd04203b7
Harish Soni <soniharish029@gmail.com>
2026-02-24 20:05:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://task-master-plzn.onrender.com/'
+        target: 'https://task-master-plzn.onrender.com/',
         changeOrigin: true,
         secure: false,
       }
